454cde579c5365919052dd69bd2460657aafb700
paulajbastos <paulajbastos@paulajbastoss-MacBook-Air-2.local>
2020-11-18 23:53:30 -0300

    trabalhando com responsividade e testando media queries

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from '../../utils/tests/helpers'
+import 'jest-styled-components'
 
 import Logo from '.'
 
@@ -46,5 +47,20 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '20rem'
     })
+  })
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    // renderizar o componente 'render'
+    // selecionar o elemento a ser testado 'screen' (queries) - getByLabel, getByTest, ...
+    // expect - assertion - comparação - análise (espero que renderize a logo branca)
+
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: `(max-width: 768px)`
+      }
+    )
   })
 })
