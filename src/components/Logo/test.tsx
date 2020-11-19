658d976bcc5633b25c2c1c0699a5988bc64b5ec6
paulajbastos <paulajbastos@paulajbastoss-MacBook-Air-2.local>
2020-11-18 23:24:06 -0300

    primeiro teste

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from '../../utils/tests/helpers'
 
 import Logo from '.'
 
@@ -7,5 +8,21 @@ describe('<Logo />', () => {
     // renderizar o componente 'render'
     // selecionar o elemento a ser testado 'screen' (queries) - getByLabel, getByTest, ...
     // expect - assertion - comparação - análise (espero que renderize a logo branca)
+
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+
+  it('should render a black label when color is passed', () => {
+    // renderizar o componente 'render'
+    // selecionar o elemento a ser testado 'screen' (queries) - getByLabel, getByTest, ...
+    // expect - assertion - comparação - análise (espero que renderize a logo branca)
+
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
