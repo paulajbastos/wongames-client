fbd906a76caa329fd848fd584995e68091c76c9b
Paula Bastos <paulabastos@Bastos-Paula-30000114.local>
2021-01-15 16:21:28 -0300

    Configurando webpack do Storybook para absolute path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,9 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-essentials']
+  addons: ['@storybook/addon-essentials'],
+  webpackFinal: (config) => {
+    //dizer para os modulos usar como local padrao o src (process.cwd() = raiz)
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
