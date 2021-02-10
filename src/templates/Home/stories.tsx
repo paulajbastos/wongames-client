01fb4bda84208fefa4a09a40a64d1119109eefbf
Paula Bastos <paulabastos@BRA-Paula-Bastos-30000114.local>
2021-02-09 21:35:17 -0300

    adicionando os primeiros componentes estaticos na home

--- src/templates/Home/stories.tsx
+++ src/templates/Home/stories.tsx
@@ -2,9 +2,8 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import Home from '.'
 
 export default {
-title: 'Home',
-component: Home
+  title: 'Home',
+  component: Home
 } as Meta
 
-export const Default: Story = () =>
-<Home />
+export const Default: Story = () => <Home />
